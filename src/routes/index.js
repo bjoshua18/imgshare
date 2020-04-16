44be3f4bdd5c044bdbe9831c2b06fd8adaa3d567
Byron Bustamante <byronbl.12@gmail.com>
2020-04-16 23:45:52 +0100

    Configuramos las rutas que tendrá la app y declaramos los controladores

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,16 @@
+const express = require('express')
+const homeController = require('../controllers/homeController')
+const imageController = require('../controllers/imageController')
+
+const router = express.Router()
+
 module.exports = app => {
-	app.get('/', (req, res) => res.send('Index page'))
+	router.get('/', homeController.index)
+	router.get('/images/:image_id', imageController.index)
+	router.post('/images', imageController.create)
+	router.post('/images/:image_id/like', imageController.like)
+	router.post('/images/:image_id/comment', imageController.comment)
+	router.delete('/images/:image_id', imageController.remove)
+
+	app.use(router)
 }
