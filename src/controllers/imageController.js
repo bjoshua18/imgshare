696c376fd9501d3c301f77d043497d84c6469200
Byron Bustamante <byronbl.12@gmail.com>
2020-04-17 06:34:52 +0100

    Implementamos la subida de imágenes desde el form

--- src/controllers/imageController.js
+++ src/controllers/imageController.js
@@ -1,11 +1,24 @@
+const path = require('path')
+const fs = require('fs-extra')
+const { getRandomString } = require('../helpers/libs')
+
 const ctrl = {}
 
-ctrl.index = (req, res) => {
+ctrl.index = async (req, res) => {
 
 }
 
 ctrl.create = (req, res) => {
-	
+	const imgName = getRandomString(6)
+	const tempPath = req.file.path
+	const ext = path.extname(req.file.originalname).toLowerCase()
+	const targetPath = path.resolve(`src/public/upload/${imgName}${ext}`)
+
+	const admitedExts = ['.png', '.jpg', '.jpeg', '.gif']
+	if(admitedExts.includes(ext))
+		await fs.rename(tempPath, targetPath)
+
+	res.send('works!')
 }
 
 ctrl.like = (req, res) => {
