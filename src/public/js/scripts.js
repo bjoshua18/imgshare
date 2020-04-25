bf5ed90b37afda3364e53b44c2712c54bb0e4d2f
Byron Bustamante <byronbl.12@gmail.com>
2020-04-25 09:52:07 +0100

    Implementamos la funcionalidad de eliminar una imagen. Eliminamos el archivo, los comentarios asociados y los datos de la imagen

--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -1,6 +1,30 @@
+$('#post-comment').hide()
+$('#btn-toggle-comment').click(e => {
+	$('#post-comment').slideToggle()
+	e.preventDefault()
+})
+
 $('#btn-like').click(function(e) {
 	const imgId = $(this).data('id')
 	$.post(`/images/${imgId}/like`)
 		.done(data => $('.likes-count').text(data.likes))
+	e.preventDefault()
+})
+
+$('#btn-delete').click(function(e) {
+	const $this = $(this)
+	const res = confirm('Are you sure you want to delete this image?')
+	if(res) {
+		const imgId = $this.data('id')
+		$.ajax({
+			url: `/images/${imgId}`,
+			type: 'DELETE'
+		})
+		.done(function(res) {
+			$this.removeClass('btn-danger').addClass('btn-success')
+			$this.find('i').removeClass('fa-times').addClass('fa-check')
+			$this.find('span').text('Deleted!')
+		})
+	}
 	e.preventDefault()
 })
